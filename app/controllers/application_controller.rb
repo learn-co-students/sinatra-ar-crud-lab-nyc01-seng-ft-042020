7926056545dcbc65a33a082cca9a40dc7d4752d9
Srishti Nath <srishti.nath@gmail.com>
2020-05-13 19:10:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+    @article = Article.new(params)
+    @article.save
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.all.find(params[:id].to_i)
+
+    redirect to :show
+  end
+
 end
