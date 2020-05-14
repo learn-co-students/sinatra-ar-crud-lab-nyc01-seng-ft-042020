ba9f98eb41d7372432282af4662591be1f254b97
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-13 22:18:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
   #update - update
   patch "/articles/:id" do
     @article = Article.find(params[:id])
-    @article.update(params[:article])
+    @article.update(params)
     redirect to "/articles/#{ @article.id }"
   end
 
