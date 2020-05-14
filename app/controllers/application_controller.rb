fdb8d1c78fbcbeeb93bb8fa12a8a0db686070464
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-13 22:07:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,55 @@ require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get '/' do
+  get "/" do
+    redirect to "/articles"
   end
+
+  #create - new
+  get "/articles/new" do
+    # @article = Article.new
+    erb :new
+  end
+
+  #create - create
+  post "/articles" do
+  @article = Article.create(params)
+  redirect to "/articles/#{ @article.id }"
+  end
+
+  #read - all/index
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  #read - show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #update - edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  #update - update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #delete
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
 end
